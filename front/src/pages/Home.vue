ffdfd710e00ba7adfb95588b5cf06ed6c25ceed7
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-10-15 11:21:00 +0200

    #2 Adding bootstrap and create home page

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -1,5 +1,44 @@
 <template>
-	<div>Hello</div>
+	<center class="container mt-5">
+		<h1 class="mb-3">
+			Front AL
+		</h1>
+		<img
+			class="m-4"
+			src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmrgxnfcba5EoAABGbbhfHhOFKB9TQVCvIZw&usqp=CAU"
+			alt="Train"
+		>
+		<ul>
+			<li>
+				<router-link to="/">
+					<div class="btn btn-primary">
+						Department
+					</div>
+				</router-link>
+			</li>
+			<li>
+				<router-link to="/">
+					<div class="btn btn-primary">
+						Department
+					</div>
+				</router-link>
+			</li>
+			<li>
+				<router-link to="/">
+					<div class="btn btn-primary">
+						Department
+					</div>
+				</router-link>
+			</li>
+			<li>
+				<router-link to="/">
+					<div class="btn btn-primary">
+						Department
+					</div>
+				</router-link>
+			</li>
+		</ul>
+	</center>
 </template>
 
 <script> 
@@ -8,3 +47,12 @@ export default {
 	name: "Home"
 };
 </script>
+
+<style>
+	li
+	{
+		display: inline;
+		list-style-type: none;
+		padding-right: 20px;
+	}
+</style>
