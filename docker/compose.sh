75a18100d30560dd5d2db47da7f6e5a16d0f7a06
AlexisLefebvre1953 <alexis.lefebvre@etu.univ-cotedazur.fr>
2020-10-23 12:40:19 +0200

    #20 #21 docker compose both front and back

---
+++ docker/compose.sh
@@ -0,0 +1,24 @@
+path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+
+# Create a .war file and store it in back/ directory
+cd ../back
+mvn clean package -DskipTests
+cd $path
+
+cp ../back/webservices/target/train-management-backend.war ./back
+
+# Create a dist directory and store it in front/ directory
+cd ../front
+npm run build
+cd $path
+
+cp -r ../front/dist ./front
+
+# build docker images 
+docker-compose build
+
+# delete generated files
+rm -rf front/dist back/train-management-backend.war
+
+# run docker 
+docker-compose up
--- docker/front/build.sh
+++ docker/front/build.sh
@@ -1,6 +1,14 @@
 path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 cd $path
 
+
+cd ../../front
+npm run build
+cd $path
+
+cp -r ../../front/dist .
 cd ../..
 
 docker build -t train-management/front-end ./docker/front
+
+rm -rf dist
