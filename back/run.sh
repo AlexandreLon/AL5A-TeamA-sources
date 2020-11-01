618f3379269fb319ccbfb3b01be4c4e6dbbb6c58
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-11-01 22:35:09 +0100

    Fix script error

--- back/run.sh
+++ back/run.sh
@@ -1,7 +1,7 @@
-SCRIPT=`realpath -s $0`
-SCRIPTPATH=`dirname $SCRIPT`
+path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+cd $path
 
-if [ -f $SCRIPTPATH/settings.xml ]; then
+if [ -f settings.xml ]; then
     mvn clean install -s settings.xml -DskipTests
 else
     mvn clean install -DskipTests
