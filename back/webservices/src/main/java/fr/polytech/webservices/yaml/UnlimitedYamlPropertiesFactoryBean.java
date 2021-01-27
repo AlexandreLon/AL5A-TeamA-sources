a9f1783780904cc1f35fb0edbe7f1417144adc99
AlexandreLon <alexandre.longordo@etu.unice.fr>
2021-01-27 15:11:09 +0100

    Debug yaml no such method #-4

--- back/webservices/src/main/java/fr/polytech/webservices/yaml/UnlimitedYamlPropertiesFactoryBean.java
+++ back/webservices/src/main/java/fr/polytech/webservices/yaml/UnlimitedYamlPropertiesFactoryBean.java
@@ -6,28 +6,11 @@ import org.yaml.snakeyaml.Yaml;
 
 public class UnlimitedYamlPropertiesFactoryBean extends YamlPropertiesFactoryBean {
 
-    private static void log(Object o) {
-        System.out.println("############################################");
-        for(int i=0; i<10; i++) {
-            System.out.println("#                                          #");
-        } 
-        System.out.println(o);
-        for(int i=0; i<10; i++) {
-            System.out.println("#                                          #");
-        } 
-        System.out.println("############################################");
-    }
-
     @Override
     protected Yaml createYaml() {
         LoaderOptions loaderOptions = new LoaderOptions();
         loaderOptions.setAllowDuplicateKeys(false);
-        log("Before1");
-        log(loaderOptions.getMaxAliasesForCollections());
-        log("Before2");
         loaderOptions.setMaxAliasesForCollections(Integer.MAX_VALUE);
-        log("After");
-        log(loaderOptions.getMaxAliasesForCollections());
         loaderOptions.setAllowRecursiveKeys(true);
 
         return new Yaml(loaderOptions);
