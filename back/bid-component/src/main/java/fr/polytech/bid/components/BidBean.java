fec959c14cb9feb86ab10e715db1e66830b52562
AlexandreLon <alexandre.longordo@etu.unice.fr>
2021-01-28 14:20:39 +0100

    #93 Create outbid method to create an offer

--- back/bid-component/src/main/java/fr/polytech/bid/components/BidBean.java
+++ back/bid-component/src/main/java/fr/polytech/bid/components/BidBean.java
@@ -1,10 +1,14 @@
 package fr.polytech.bid.components;
 
 import fr.polytech.bid.errors.BidNotFoundException;
+import fr.polytech.bid.errors.SupplierNotFoundException;
 import fr.polytech.bid.models.Bid;
+import fr.polytech.bid.models.Offer;
 import fr.polytech.bid.models.BidStatus;
 import fr.polytech.bid.models.Supplier;
 import fr.polytech.bid.repositories.BidRepository;
+import fr.polytech.bid.repositories.OfferRepository;
+import fr.polytech.bid.repositories.SupplierRepository;
 import fr.polytech.task.models.Task;
 
 import org.springframework.stereotype.Component;
@@ -13,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -22,10 +25,16 @@ import java.util.Optional;
 @ComponentScan("fr.polytech.bid.repositories")
 @EntityScan("fr.polytech.bid.models")
 @EnableJpaRepositories("fr.polytech.bid.repositories")
-public class BidBean implements BidViewer, BidCreator {
+public class BidBean implements BidViewer, BidCreator, BidProposer {
 
     @Autowired
     private BidRepository bidRepository;
+
+    @Autowired
+    private SupplierRepository supplierReposity;
+
+    @Autowired
+    private OfferRepository offerRepository;
 
     @Override
     public List<Bid> getBids() {
@@ -43,10 +52,33 @@ public class BidBean implements BidViewer, BidCreator {
     @Override
     public Bid createBid(Task task, List<Supplier> suppliers, Date desiredDate) {
         Bid bid = new Bid();
-        bid.setName(task.getName()); //TODO Maybe remove name or how to choose the name of bid ?
+        bid.setName(task.getName()); // TODO Maybe remove name or how to choose the name of bid ?
         bid.setTask(task);
         bid.setDesiredDate((desiredDate));
         bid.setStatus(BidStatus.ONGOING);
         return bidRepository.save(bid);
+    }
+
+    @Override
+    public Offer outbid(long bidId, long supplierId, double price, Date proposedDate) throws BidNotFoundException,
+            SupplierNotFoundException {
+        Offer offer = new Offer();
+
+        Optional<Bid> optBid = bidRepository.findById(bidId);
+        if (!optBid.isPresent())
+            throw new BidNotFoundException();
+        Bid bid = optBid.get();
+
+        Optional<Supplier> optSupplier = supplierReposity.findById(supplierId);
+        if (!optSupplier.isPresent())
+            throw new SupplierNotFoundException();
+        Supplier supplier = optSupplier.get();
+        
+        offer.setBid(bid);
+        offer.setPrice(price);
+        offer.setSupplier(supplier);
+        offer.setProposedDate(proposedDate);
+
+        return offerRepository.save(offer);
     }
 }
---
+++ back/bid-component/src/main/java/fr/polytech/bid/errors/SupplierNotFoundException.java
@@ -0,0 +1,9 @@
+package fr.polytech.bid.errors;
+
+public class SupplierNotFoundException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 2L;
+}
