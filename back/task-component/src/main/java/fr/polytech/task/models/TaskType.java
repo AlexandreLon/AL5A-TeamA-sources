9f22c66a355db95f228ba42e1ede444c7ab002bd
YounesAbdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2021-02-05 08:48:56 +0100

    Added enum string values for task types.

--- back/task-component/src/main/java/fr/polytech/task/models/TaskType.java
+++ back/task-component/src/main/java/fr/polytech/task/models/TaskType.java
@@ -1,5 +1,18 @@
 package fr.polytech.task.models;
 
 public enum TaskType {
-    VERIFICATION,CLEANING,REPLACING
+    VERIFICATION("verification"),
+    CLEANING("cleaning"),
+    REPLACING("replacing"),
+    ;
+
+    private String type;
+
+    TaskType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
 }
